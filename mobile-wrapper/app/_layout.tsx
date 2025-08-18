6a24ff82b731a94e60ee1f089ecf29efdf5c84e9
DeZheng Chang <dcha0082@student.monash.edu>
2025-08-18 19:02:57 +1000

    Setup android files

--- mobile-wrapper/app/_layout.tsx
+++ mobile-wrapper/app/_layout.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, SafeAreaView } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function AppLayout() {
-  const webAppUrl = 'http://<your-ip>:3000'; 
+  const webAppUrl = 'http://192.168.1.111:3000'; 
 
   return (
     <SafeAreaView style={styles.container}>
