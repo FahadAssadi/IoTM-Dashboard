11ab6f8bc82c50a07b48ab3e16a929ade4cf1ec2
Kevin Cody <kcod0001@student.monash.edu>
2025-10-17 23:52:35 +1100

    Further merge conflict fixes

--- app/backend/Data/ApplicationDbContext.cs
+++ app/backend/Data/ApplicationDbContext.cs
@@ -23,7 +23,6 @@ namespace IoTM.Data
         public DbSet<ScreeningGuideline> ScreeningGuidelines { get; set; }
         public DbSet<UserScreening> UserScreenings { get; set; }
         public DbSet<HealthAlert> HealthAlerts { get; set; }
-        public DbSet<NewsArticle> NewsArticles { get; set; }
         public DbSet<HealthSegmentBPM> HealthSegmentBPMs { get; set; }
         public DbSet<HealthSegmentSpO2> HealthSegmentSpO2s { get; set; }
         public DbSet<HealthSegmentBloodPressure> HealthSegmentBloodPressures { get; set; }
